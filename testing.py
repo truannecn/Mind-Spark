3122d983e301d631f65927b735a9ffe1219ad9ca
andrewji913 <andrewyjiang@gmail.com>
2024-11-15 22:15:48 -0500

    started writing classes
    fire

--- testing.py
+++ testing.py
@@ -1,1 +1,15 @@
-print('hi')
+
+
+class Account():
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+class Calender():
+    def __init__(self):
+        self.calender = {}
+
+class DailyEntry():
+    def __init__(self, user):
+        self.journalEntry = ''
+        self.mood = None
