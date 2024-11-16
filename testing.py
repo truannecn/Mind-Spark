b1d65c527e473a90719a4c836108fddd88b6256e
michelle <39465865+mimi0526@users.noreply.github.com>
2024-11-16 00:51:06 -0500

    hi
    hi

--- testing.py
+++ testing.py
@@ -1,15 +1,67 @@
 from cmu_graphics import *
 import string
+import calendar
 
+######
+# ON APP START
+######
 def onAppStart(app):
+    app.page = 'login'
+    app.users = []
+
+    #login page app variables
+    app.username = ''
+    app.password = ''
+    app.currentUser = None
+
+    #user variables
+    app.calender = UserCalender()
+
+    #mood app variables
     app.faceR = 10
     app.moodFaces = []
+
+    #textBox app variables
     app.inBox = False
     app.entry = ''
-    app.page = 1
     app.textBoxLeft = 50
     app.textBoxTop = 50
     app.entryList = ["aldkfjadslkfjasklfjlaskjf"]
+
+
+
+######
+# ON KEY PRESS
+######
+def onKeyPress(app, key):
+    if app.inBox:
+        if key == 'space':
+            app.entry += " "
+        elif key == 'backspace':
+            app.entry = app.entry[:-1]
+        elif key.isalpha() or key.isdigit() or key in string.punctuation:
+            app.entry += key
+        else:
+            app.entry = app.entry
+
+#####
+# ON MOUSE PRESS
+#####
+def onMousePress(app, mouseX, mouseY):
+    if 50<=mouseX<=app.width-50 and 50<=mouseY<=app.height/3:
+        app.inBox = True
+
+#####
+# REDRAW ALL
+#####
+def redrawAll(app):
+    if app.page == 'journal entry':
+        makeTextBox(app)
+        updateTextBox(app)
+    elif app.page == 'mood':
+        drawMoods(app)
+    elif app.page == 'calender view':
+        drawCalender(app, app.calender)
 
 def makeTextBox(app):
     drawRect(app.textBoxLeft, app.textBoxTop, app.width-100, app.height/3, border='black', fill=None)
@@ -24,37 +76,23 @@ def updateTextBox(app):
 
     drawLabel(app.entry, 57, currentLine + 15, align='left', fill='black',size=15)
 
-def onKeyPress(app, key):
-    if app.inBox:
-        if key == 'space':
-            app.entry += " "
-        elif key == 'backspace':
-            app.entry = app.entry[:-1]
-        elif key.isalpha() or key.isdigit() or key in string.punctuation:
-            app.entry += key
-        else:
-            app.entry = app.entry
+def drawMoods(app):
+    moods
 
-        
-
-def onMousePress(app, mouseX, mouseY):
-    if 50<=mouseX<=app.width-50 and 50<=mouseY<=app.height/3:
-        app.inBox = True
-
-
-def redrawAll(app):
-    if app.page == 1:
-        makeTextBox(app)
-        updateTextBox(app)
+def drawCalender(app):
+    months = {'January': 31, 'February': (28, 29), 'March': 31, 'April': 30, 'May': 31, 'June': 30, 'July': 31, 'August': 31, 'September': 30, 'October': 31, 'November': 30, 'December': 31}
+    boxWidth = app.width // 7
+    daysOfWeek = ['Sunday', 'Monday', '']
 
 
 class Account():
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        app.name = None
+        self.name = None
+        self.calender = UserCalender()
 
-class Calender():
+class UserCalender():
     def __init__(self):
         self.calender = {}
 
